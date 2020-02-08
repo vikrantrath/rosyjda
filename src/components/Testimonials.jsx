98c532d6a6c3f0bb3ff9d494c77cbc36179135d3
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-02-08 15:14:38 +0530

    Added Mision and Vision

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,75 +1,72 @@
-import React, { Component } from 'react';
-import Icofont from 'react-icofont';
+import React, { Component } from "react";
+import Icofont from "react-icofont";
 import OwlCarousel from "react-owl-carousel3";
 import PropTypes from "prop-types";
 
 class Testimonials extends Component {
   render() {
     //Testimonials loop start
-    const testimonialsitem = this.props.testimonialsData.map((testimonials, index) => (
+    const testimonialsitem = this.props.testimonialsData.map(
+      (testimonials, index) => (
         <div className="single-testimonial-item text-center" key={index}>
-            <Icofont icon="icofont-quote-left" />
-            <p>{testimonials.Content}</p>
-
-            <div className="client-profile">
-                <img src={testimonials.clientImage} alt="client-one" />
-            </div>
-            
-            <div className="client-info">
-                <h3>{testimonials.Name}</h3>
-                <span>{testimonials.Profession}</span>
-            </div>
+          <Icofont icon="icofont-quote-left" />
+          <p>{testimonials.Content}</p>
+          <div className="client-info">
+            <h3>{testimonials.Name}</h3>
+          </div>
         </div>
-    ));
+      )
+    );
     //Testimonials loop END
     return (
-        <React.Fragment>
-            <section className="testimonials ptb-100">
-                <div className="container">
-                    <OwlCarousel
-                        className="owl-theme testimonial-slides"
-                        items={1}
-                        nav= {true}
-                        dots= {false}
-                        autoplay= {false}
-                        loop= {true}
-                        autoplayHoverPause= {true}
-                        smartSpeed= {1000}
-                        navText= {[
-                            "<i class='icofont-arrow-left'></i>", 
-                            "<i class='icofont-arrow-right'></i>"
-                        ]}
-                    >
-                            {testimonialsitem}
-                    </OwlCarousel>
-                </div>
-            </section>
-        </React.Fragment>
+      <React.Fragment>
+        <section className="testimonials ptb-100">
+          <div className="container">
+            <OwlCarousel
+              className="owl-theme testimonial-slides"
+              items={1}
+              nav={true}
+              dots={false}
+              autoplay={false}
+              loop={true}
+              autoplayHoverPause={true}
+              smartSpeed={1000}
+              navText={[
+                "<i class='icofont-arrow-left'></i>",
+                "<i class='icofont-arrow-right'></i>"
+              ]}
+            >
+              {testimonialsitem}
+            </OwlCarousel>
+          </div>
+        </section>
+      </React.Fragment>
     );
   }
 }
 
 //Props Types
 Testimonials.propTypes = {
-    testimonialsData: PropTypes.array
+  testimonialsData: PropTypes.array
 };
 
 //Default Props
-Testimonials.defaultProps = { 
-    testimonialsData: [
-        {
-            clientImage: require("../assets/img/client-one.png"),
-            Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum sagittis nulla, non vehicula mauris rutrum vitae. Sed non consequat dolor. Cras in odio augue.",
-            Name: "Jason Statham",
-            Profession: "Founder & Director",
-        },
-        {
-            clientImage: require("../assets/img/client-one.png"),
-            Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum sagittis nulla, non vehicula mauris rutrum vitae. Sed non consequat dolor. Cras in odio augue.",
-            Name: "Jason Statham",
-            Profession: "Founder & Director",
-        },
-    ]
+Testimonials.defaultProps = {
+  testimonialsData: [
+    {
+      clientImage: require("../assets/img/client-one.png"),
+      Content:"To help our clients to meet their goals through our professionals, services and solutions.",
+      Name: "Our Mission",
+      Profession: "Founder & Director"
+    },
+    {
+      clientImage: require("../assets/img/client-one.png"),
+      Content:
+      "To provide the best professional solutions to fulfil the client's business requirements, guiding at our best.",
+      Name: "Our Vision",
+      Profession: "Founder & Director"
+    }
+  ]
 };
 
 export default Testimonials;
--- src/pages/HomeThree.jsx
+++ src/pages/HomeThree.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //Import Component
 import NavBar from "../components/NavBar";
@@ -19,46 +19,35 @@ import Partner from "../components/Partner";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
-
 class HomeThree extends Component {
   render() {
     return (
-        <React.Fragment>
-            {/* NavBar: src/components/NavBar.jsx */}
-            <NavBar pageName="home" />
-            {/* BannerThree: src/components/banner/BannerThree.jsx */}
-            <BannerThree />
-            {/* Feature: src/components/Feature.jsx */}
-            <Feature />
-            {/* Services: src/components/Services.jsx */}
-            <Services />
-            {/* WelcomeServices: src/components/WelcomeServices.jsx */}
-            <WelcomeServices />
-            {/* Works: src/components/Works.jsx */}
-            <Works />
-            {/* About: src/components/About.jsx */}
-            <About />
-            {/* Team: src/components/Team.jsx */}
-            <Team />
-            {/* Blog: src/components/Blog.jsx */}
-            <Blog />
-            {/* VideoArea: src/components/VideoArea.jsx */}
-            <VideoArea />
-            {/* Pricing: src/components/Pricing.jsx */}
-            <Pricing />
-            {/* FunFacts: src/components/FunFacts.jsx */}
-            <FunFacts />
-            {/* Testimonials: src/components/Testimonials.jsx */}
-            <Testimonials />
-            {/* FAQ: src/components/FAQ.jsx */}
-            <FAQ />
-            {/* Partner: src/components/Partner.jsx */}
-            <Partner />
-            {/* Contact: src/components/Contact.jsx */}
-            <Contact />
-            {/* Footer: src/components/Footer.jsx */}
-            <Footer />
-        </React.Fragment>
+      <React.Fragment>
+        {/* NavBar: src/components/NavBar.jsx */}
+        <NavBar pageName="home" />
+        {/* BannerThree: src/components/banner/BannerThree.jsx */}
+        <BannerThree />
+        {/* Feature: src/components/Feature.jsx */}
+        <Feature />
+        {/* Services: src/components/Services.jsx */}
+        <Services />
+        {/* WelcomeServices: src/components/WelcomeServices.jsx */}
+        <WelcomeServices />
+        {/* About: src/components/About.jsx */}
+        <About />
+        {/* Testimonials: src/components/Testimonials.jsx */}
+        <Testimonials />
+        {/* Team: src/components/Team.jsx */}
+        <Team />
+        {/* FAQ: src/components/FAQ.jsx */}
+        <FAQ />
+        {/* Partner: src/components/Partner.jsx */}
+        <Partner />
+        {/* Contact: src/components/Contact.jsx */}
+        <Contact />
+        {/* Footer: src/components/Footer.jsx */}
+        <Footer />
+      </React.Fragment>
     );
   }
 }
